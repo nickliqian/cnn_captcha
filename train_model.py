d9d82b943fd5210c0f8f0f6a0216c06970fffe2c
liqian <nickliqian@outlook.com>
2019-04-09 17:25:17 +0800

    dev init commit and rebase recognoze object code

--- train_model.py
+++ train_model.py
@@ -8,6 +8,8 @@ import random
 import os
 from sample import sample_conf
 from tensorflow.python.framework.errors_impl import NotFoundError
+from network import CNN
+
 
 # 设置以下环境变量可开启CPU识别
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -17,23 +19,24 @@ class TrainError(Exception):
     pass
 
 
-class TrainModel(object):
-    def __init__(self, img_path, char_set, model_save_dir, verify=False):
-        # 模型路径
-        self.model_save_dir = model_save_dir
-
+class TrainModel(CNN):
+    def __init__(self, train_img_path, verify_img_path, char_set, model_save_dir, verify=False):
         # 打乱文件顺序+校验图片格式
-        self.img_path = img_path
-        self.img_list = os.listdir(img_path)
+        self.train_img_path = train_img_path
+        self.train_images_list = os.listdir(train_img_path)
         # 校验格式
         if verify:
             self.confirm_image_suffix()
         # 打乱文件顺序
         random.seed(time.time())
-        random.shuffle(self.img_list)
+        random.shuffle(self.train_images_list)
+
+        # 验证集文件
+        self.verify_img_path = verify_img_path
+        self.verify_images_list = os.listdir(verify_img_path)
 
         # 获得图片宽高和字符长度基本信息
-        label, captcha_array = self.gen_captcha_text_image(self.img_list[0])
+        label, captcha_array = self.gen_captcha_text_image(train_img_path, self.train_images_list[0])
 
         captcha_shape = captcha_array.shape
         captcha_shape_len = len(captcha_shape)
@@ -46,27 +49,14 @@ class TrainModel(object):
             raise TrainError("图片转换为矩阵时出错，请检查图片格式")
 
         # 初始化变量
-        # 图片尺寸
-        self.image_height = image_height
-        self.image_width = image_width
-        # 验证码长度（位数）
-        self.max_captcha = len(label)
-        # 验证码字符类别
-        self.char_set = char_set
-        self.char_set_len = len(char_set)
+        super(TrainModel, self).__init__(image_height, image_width, len(label), char_set, model_save_dir)
 
         # 相关信息打印
         print("-->图片尺寸: {} X {}".format(image_height, image_width))
         print("-->验证码长度: {}".format(self.max_captcha))
         print("-->验证码共{}类 {}".format(self.char_set_len, char_set))
-        print("-->使用测试集为 {}".format(img_path))
-
-        # tf初始化占位符
-        self.X = tf.placeholder(tf.float32, [None, image_height * image_width])  # 特征向量
-        self.Y = tf.placeholder(tf.float32, [None, self.max_captcha * self.char_set_len])  # 标签
-        self.keep_prob = tf.placeholder(tf.float32)  # dropout值
-        self.w_alpha = 0.01
-        self.b_alpha = 0.1
+        print("-->使用测试集为 {}".format(train_img_path))
+        print("-->使验证集为 {}".format(verify_img_path))
 
         # test model input and output
         print(">>> Start model test")
@@ -74,55 +64,11 @@ class TrainModel(object):
         print(">>> input batch images shape: {}".format(batch_x.shape))
         print(">>> input batch labels shape: {}".format(batch_y.shape))
 
-    def gen_captcha_text_image(self, img_name):
-        """
-        返回一个验证码的array形式和对应的字符串标签
-        :return:tuple (str, numpy.array)
-        """
-        # 标签
-        label = img_name.split("_")[0]
-        # 文件
-        img_file = os.path.join(self.img_path, img_name)
-        captcha_image = Image.open(img_file)
-        captcha_array = np.array(captcha_image)  # 向量化
-        return label, captcha_array
-
-    @staticmethod
-    def convert2gray(img):
-        """
-        图片转为灰度图，如果是3通道图则计算，单通道图则直接返回
-        :param img:
-        :return:
-        """
-        if len(img.shape) > 2:
-            r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-            gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-            return gray
-        else:
-            return img
-
-    def text2vec(self, text):
-        """
-        转标签为oneHot编码
-        :param text: str
-        :return: numpy.array
-        """
-        text_len = len(text)
-        if text_len > self.max_captcha:
-            raise ValueError('验证码最长{}个字符'.format(self.max_captcha))
-
-        vector = np.zeros(self.max_captcha * self.char_set_len)
-
-        for i, ch in enumerate(text):
-            idx = i * self.char_set_len + self.char_set.index(ch)
-            vector[idx] = 1
-        return vector
-
     def get_batch(self, n, size=128):
         batch_x = np.zeros([size, self.image_height * self.image_width])  # 初始化
         batch_y = np.zeros([size, self.max_captcha * self.char_set_len])  # 初始化
 
-        max_batch = int(len(self.img_list) / size)
+        max_batch = int(len(self.train_images_list) / size)
         # print(max_batch)
         if max_batch - 1 < 0:
             raise TrainError("训练集图片数量需要大于每批次训练的图片数量")
@@ -130,11 +76,26 @@ class TrainModel(object):
             n = n % max_batch
         s = n * size
         e = (n + 1) * size
-        this_batch = self.img_list[s:e]
+        this_batch = self.train_images_list[s:e]
         # print("{}:{}".format(s, e))
 
         for i, img_name in enumerate(this_batch):
-            label, image_array = self.gen_captcha_text_image(img_name)
+            label, image_array = self.gen_captcha_text_image(self.train_img_path, img_name)
+            image_array = self.convert2gray(image_array)  # 灰度化图片
+            batch_x[i, :] = image_array.flatten() / 255  # flatten 转为一维
+            batch_y[i, :] = self.text2vec(label)  # 生成 oneHot
+        return batch_x, batch_y
+
+    def get_verify_batch(self, size=100):
+        batch_x = np.zeros([size, self.image_height * self.image_width])  # 初始化
+        batch_y = np.zeros([size, self.max_captcha * self.char_set_len])  # 初始化
+
+        verify_images = []
+        for i in range(size):
+            verify_images.append(random.choice(self.verify_images_list))
+
+        for i, img_name in enumerate(verify_images):
+            label, image_array = self.gen_captcha_text_image(self.verify_img_path, img_name)
             image_array = self.convert2gray(image_array)  # 灰度化图片
             batch_x[i, :] = image_array.flatten() / 255  # flatten 转为一维
             batch_y[i, :] = self.text2vec(label)  # 生成 oneHot
@@ -143,57 +104,12 @@ class TrainModel(object):
     def confirm_image_suffix(self):
         # 在训练前校验所有文件格式
         print("开始校验所有图片后缀")
-        for index, img_name in enumerate(self.img_list):
+        for index, img_name in enumerate(self.train_images_list):
             print("{} image pass".format(index), end='\r')
             if not img_name.endswith(sample_conf['image_suffix']):
                 raise TrainError('confirm images suffix：you request [.{}] file but get file [{}]'
                                  .format(sample_conf['image_suffix'], img_name))
         print("所有图片格式校验通过")
-
-    def model(self):
-        x = tf.reshape(self.X, shape=[-1, self.image_height, self.image_width, 1])
-        print(">>> input x: {}".format(x))
-
-        # 卷积层1
-        wc1 = tf.get_variable(name='wc1', shape=[3, 3, 1, 32], dtype=tf.float32,
-                              initializer=tf.contrib.layers.xavier_initializer())
-        bc1 = tf.Variable(self.b_alpha * tf.random_normal([32]))
-        conv1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, wc1, strides=[1, 1, 1, 1], padding='SAME'), bc1))
-        conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        conv1 = tf.nn.dropout(conv1, self.keep_prob)
-
-        # 卷积层2
-        wc2 = tf.get_variable(name='wc2', shape=[3, 3, 32, 64], dtype=tf.float32,
-                              initializer=tf.contrib.layers.xavier_initializer())
-        bc2 = tf.Variable(self.b_alpha * tf.random_normal([64]))
-        conv2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv1, wc2, strides=[1, 1, 1, 1], padding='SAME'), bc2))
-        conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        conv2 = tf.nn.dropout(conv2, self.keep_prob)
-
-        # 卷积层3
-        wc3 = tf.get_variable(name='wc3', shape=[3, 3, 64, 128], dtype=tf.float32,
-                              initializer=tf.contrib.layers.xavier_initializer())
-        bc3 = tf.Variable(self.b_alpha * tf.random_normal([128]))
-        conv3 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv2, wc3, strides=[1, 1, 1, 1], padding='SAME'), bc3))
-        conv3 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        conv3 = tf.nn.dropout(conv3, self.keep_prob)
-        print(">>> convolution 3: ", conv3.shape)
-        next_shape = conv3.shape[1] * conv3.shape[2] * conv3.shape[3]
-
-        # 全连接层1
-        wd1 = tf.get_variable(name='wd1', shape=[next_shape, 1024], dtype=tf.float32,
-                              initializer=tf.contrib.layers.xavier_initializer())
-        bd1 = tf.Variable(self.b_alpha * tf.random_normal([1024]))
-        dense = tf.reshape(conv3, [-1, wd1.get_shape().as_list()[0]])
-        dense = tf.nn.relu(tf.add(tf.matmul(dense, wd1), bd1))
-        dense = tf.nn.dropout(dense, self.keep_prob)
-
-        # 全连接层2
-        wout = tf.get_variable('name', shape=[1024, self.max_captcha * self.char_set_len], dtype=tf.float32,
-                               initializer=tf.contrib.layers.xavier_initializer())
-        bout = tf.Variable(self.b_alpha * tf.random_normal([self.max_captcha * self.char_set_len]))
-        y_predict = tf.add(tf.matmul(dense, wout), bout)
-        return y_predict
 
     def train_cnn(self):
         y_predict = self.model()
@@ -228,24 +144,35 @@ class TrainModel(object):
             step = 1
             for i in range(3000):
                 batch_x, batch_y = self.get_batch(i, size=128)
-                _, cost_ = sess.run([optimizer, cost], feed_dict={self.X: batch_x, self.Y: batch_y, self.keep_prob: 0.75})
+                # 梯度下降训练
+                _, cost_ = sess.run([optimizer, cost],
+                                    feed_dict={self.X: batch_x, self.Y: batch_y, self.keep_prob: 0.75})
                 if step % 10 == 0:
+                    # 基于训练集的测试
                     batch_x_test, batch_y_test = self.get_batch(i, size=100)
                     acc_char = sess.run(accuracy_char_count, feed_dict={self.X: batch_x_test, self.Y: batch_y_test, self.keep_prob: 1.})
                     acc_image = sess.run(accuracy_image_count, feed_dict={self.X: batch_x_test, self.Y: batch_y_test, self.keep_prob: 1.})
-                    print("第{}次训练 >>> 字符准确率为 {} 图片准确率为 {} >>> loss {}".format(step, acc_char, acc_image, cost_))
-                    # 图片准确率达到99%后保存并停止
+                    print("第{}次训练 >>> ".format(step))
+                    print("[训练集] 字符准确率为 {:.5f} 图片准确率为 {:.5f} >>> loss {:.10f}".format(acc_char, acc_image, cost_))
+                    # 基于验证集的测试
+                    batch_x_verify, batch_y_verify = self.get_verify_batch(size=100)
+                    acc_char = sess.run(accuracy_char_count, feed_dict={self.X: batch_x_verify, self.Y: batch_y_verify, self.keep_prob: 1.})
+                    acc_image = sess.run(accuracy_image_count, feed_dict={self.X: batch_x_verify, self.Y: batch_y_verify, self.keep_prob: 1.})
+                    print("[验证集] 字符准确率为 {:.5f} 图片准确率为 {:.5f} >>> loss {:.10f}".format(acc_char, acc_image, cost_))
+                    # 准确率达到99%后保存并停止
                     if acc_image > 0.99:
                         saver.save(sess, self.model_save_dir)
+                        print("验证集准确率达到99%，保存模型成功")
                         break
                 # 每训练500轮就保存一次
                 if i % 500 == 0:
                     saver.save(sess, self.model_save_dir)
+                    print("定时保存模型成功")
                 step += 1
             saver.save(sess, self.model_save_dir)
 
     def recognize_captcha(self):
-        label, captcha_array = self.gen_captcha_text_image(random.choice(self.img_list))
+        label, captcha_array = self.gen_captcha_text_image(self.train_img_path, random.choice(self.train_images_list))
 
         f = plt.figure()
         ax = f.add_subplot(111)
@@ -276,9 +203,10 @@ class TrainModel(object):
 
 def main():
     train_image_dir = sample_conf["train_image_dir"]
+    verify_image_dir = sample_conf["test_image_dir"]
     char_set = sample_conf["char_set"]
     model_save_dir = sample_conf["model_save_dir"]
-    tm = TrainModel(train_image_dir, char_set, model_save_dir, verify=False)
+    tm = TrainModel(train_image_dir, verify_image_dir, char_set, model_save_dir, verify=False)
     tm.train_cnn()  # 开始训练模型
     # tm.recognize_captcha()  # 识别图片示例
 
